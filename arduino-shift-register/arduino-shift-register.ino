f370dfcfce86282c98efe28a56d180979c30ea21
Ryo Suzuki <ryosuzk@gmail.com>
2017-02-24 11:40:27 -0700

    Update

--- arduino-shift-register/arduino-shift-register.ino
+++ arduino-shift-register/arduino-shift-register.ino
@@ -5,11 +5,21 @@ int clockPin = 12;
 ////Pin connected to DS of 74HC595
 int dataPin = 11;
 
+const int PWMA = 3; 
+const int STBY = 2; 
+const int PWMB = 5; 
+
+
 void setup() {
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  
   Serial.begin(9600); 
   Serial.println("Hello World");
 }
@@ -18,25 +28,59 @@ void loop() {
   // count from 0 to 255 and display the number 
   // on the LEDs
 
+  digitalWrite(STBY, HIGH);
+  digitalWrite(PWMA, 100); 
+  digitalWrite(PWMB, 100); 
+
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, 1);  
   digitalWrite(latchPin, HIGH);
+  Serial.println(1);
+
   delay(1000);
 
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, 2);  
   digitalWrite(latchPin, HIGH);
+  Serial.println(5);
+
   delay(1000);
 
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, 4);  
   digitalWrite(latchPin, HIGH);
+  Serial.println(8);
+
   delay(1000);
 
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 2);  
+  shiftOut(dataPin, clockPin, MSBFIRST, 8);  
   digitalWrite(latchPin, HIGH);
+  Serial.println(5);
+
   delay(1000);
+
+
+
+//  digitalWrite(latchPin, LOW);
+//  shiftOut(dataPin, clockPin, MSBFIRST, 1);  
+//  digitalWrite(latchPin, HIGH);
+//  delay(1000);
+//
+//  digitalWrite(latchPin, LOW);
+//  shiftOut(dataPin, clockPin, MSBFIRST, 2);  
+//  digitalWrite(latchPin, HIGH);
+//  delay(1000);
+//
+//  digitalWrite(latchPin, LOW);
+//  shiftOut(dataPin, clockPin, MSBFIRST, 4);  
+//  digitalWrite(latchPin, HIGH);
+//  delay(1000);
+//
+//  digitalWrite(latchPin, LOW);
+//  shiftOut(dataPin, clockPin, MSBFIRST, 2);  
+//  digitalWrite(latchPin, HIGH);
+//  delay(1000);
 
 //  digitalWrite(latchPin, LOW);
 //  shiftOut(dataPin, clockPin, MSBFIRST, 4);  
