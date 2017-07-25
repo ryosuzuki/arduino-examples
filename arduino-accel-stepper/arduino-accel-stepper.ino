950ce40e1d3facedb639aca32d9587d0388ccafd
Ryo Suzuki <ryosuzk@gmail.com>
2017-07-24 18:11:09 -0600

    Update

--- arduino-accel-stepper/arduino-accel-stepper.ino
+++ arduino-accel-stepper/arduino-accel-stepper.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h>
 
 // Define some steppers and the pins the will use
-AccelStepper stepper1; // Defaults to 4 pins on 2, 3, 4, 5
+AccelStepper stepper1(4, 2, 3, 4, 5);
 AccelStepper stepper2(4, 8, 9, 10, 11);
 
 void setup() {  
